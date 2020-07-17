bdb3fd41f85f80b7ceab301661617b4c2cda3089
Nick Chapman <nic@amadido.co.uk>
2020-07-17 12:06:57 +0100

    show converging performance numbers

--- perf/nfib-inter.c
+++ perf/nfib-inter.c
@@ -17,6 +17,9 @@ int main() {
 
   int n = 25;
 
+  long acc_result = 0;
+  double acc_duration_us = 0;
+
   for (;;) {
     static char arg[50];
     sprintf(arg,"%d",n);
@@ -25,12 +28,19 @@ int main() {
     long result = nfib(arg);
     clock_t end = clock();
 
-    double duration = (end-start)/1000000.0;
-    double nfibs_per_ms = (double)result / (end - start);
+    double duration_us = (end-start);
+    double duration_s = duration_us/1000000.0;
+    double nfibs_per_us = (double)result / duration_us;
 
-    printf("n = '%d', res = %8ld, duration(s) = %.3g, speed(nfibs/us) = %.3g\n",
-           n, result, duration, nfibs_per_ms);
+    // show (hopefully) converging numbers
+    acc_result += result;
+    acc_duration_us += duration_us;
+    double conv_nfibs_per_us = (double)acc_result / acc_duration_us;
 
-    if (duration < 0.2) n++;
+    printf("n = %d, res = %8ld, time(s) = %.03g, speed(nfibs/us) = %.3g (%.3g)\n",
+           n, result, duration_s, nfibs_per_us, conv_nfibs_per_us);
+
+    if (duration_s < 0.2) n++;
+
   }
 }
